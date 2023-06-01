f7af02a9b8296b4e5e6ef70cb4c48cc164d0c49c
dobroStas <Konanykhin.Stanislav@gmail.com>
2023-06-01 18:06:21 +0300

    ex 10 & 14

---
+++ Lections/first/master.py
@@ -0,0 +1,23 @@
+# print(5)
+# print(5)
+print(5)
+# print(5)
+
+print(5)
+# print(5)
+n = 1
+print(type(n))
+
+a = 2
+b = 3.14
+c = 'sdd'
+print(f"{a} - {b} - {c}")
+print("{} - {} - {}".format(a,b,c))
+
+print('Введите первое число:')
+a = input()
+
+b = input('Введите ворое число: ')
+
+v = 4.48545
+print(round(v, 3))
